edc23a34a4d8314dca6af83cce0f43581d314b36
Pedro Oliveira <pjmc.oliveira@gmail.com>
2021-10-03 18:15:57 -0400

    Added Parser

---
+++ src/Parser.test.ts
@@ -0,0 +1,222 @@
+import { Token } from "./Lexer"
+import { Apply, Binding, Bool, If, Lambda, Let, Num, parse, Var } from "./Parser"
+
+
+describe('Parser', () => {
+  it('parses an empty list', () => {
+    expect(parse([])).toEqual([])
+  })
+
+  it('parses two bindings', () => {
+    const tokens = [
+      Token.Def,
+      Token.Var('x'),
+      Token.Equal,
+      Token.Var('x'),
+
+      Token.Def,
+      Token.Var('y'),
+      Token.Equal,
+      Token.Var('y'),
+    ]
+    const bindings = parse(tokens)
+    expect(bindings).toEqual([
+      new Binding('x', new Var('x')),
+      new Binding('y', new Var('y')),
+    ])
+  })
+
+  it('parses a variable expression', () => {
+    const tokens = [
+      Token.Def,
+      Token.Var('x'),
+      Token.Equal,
+      Token.Var('x'),
+    ]
+    const bindings = parse(tokens)
+    const expr = bindings[0].expr
+    expect(expr).toEqual(
+      new Var('x')
+    )
+  })
+
+  it('parses a number expression', () => {
+    const tokens = [
+      Token.Def,
+      Token.Var('x'),
+      Token.Equal,
+      Token.Num(123),
+    ]
+    const bindings = parse(tokens)
+    const expr = bindings[0].expr
+    expect(expr).toEqual(
+      new Num(123)
+    )
+  })
+
+  it('parses a true boolean expression', () => {
+    const tokens = [
+      Token.Def,
+      Token.Var('x'),
+      Token.Equal,
+      Token.Bool(true),
+    ]
+    const bindings = parse(tokens)
+    const expr = bindings[0].expr
+    expect(expr).toEqual(
+      new Bool(true)
+    )
+  })
+
+  it('parses a false boolean expression', () => {
+    const tokens = [
+      Token.Def,
+      Token.Var('x'),
+      Token.Equal,
+      Token.Bool(false),
+    ]
+    const bindings = parse(tokens)
+    const expr = bindings[0].expr
+    expect(expr).toEqual(
+      new Bool(false)
+    )
+  })
+
+  it('parses an if expression', () => {
+    const tokens = [
+      Token.Def,
+      Token.Var('x'),
+      Token.Equal,
+      Token.If,
+      Token.Bool(true),
+      Token.Then,
+      Token.Num(123),
+      Token.Else,
+      Token.Var('x')
+    ]
+    const bindings = parse(tokens)
+    const expr = bindings[0].expr
+    expect(expr).toEqual(
+      new If(
+        new Bool(true),
+        new Num(123),
+        new Var('x')
+      )
+    )
+  })
+
+  it('parses a let expression', () => {
+    const tokens = [
+      Token.Def,
+      Token.Var('x'),
+      Token.Equal,
+      Token.Let,
+      Token.Var('x'),
+      Token.Equal,
+      Token.Bool(true),
+      Token.In,
+      Token.Num(123),
+    ]
+    const bindings = parse(tokens)
+    const expr = bindings[0].expr
+    expect(expr).toEqual(
+      new Let(
+        'x',
+        new Bool(true),
+        new Num(123)
+      )
+    )
+  })
+
+  it('parses a lambda expression', () => {
+    const tokens = [
+      Token.Def,
+      Token.Var('f'),
+      Token.Equal,
+      Token.BackSlash,
+      Token.Var('x'),
+      Token.Num(123),
+    ]
+    const bindings = parse(tokens)
+    const expr = bindings[0].expr
+    expect(expr).toEqual(
+      new Lambda(
+        'x',
+        new Num(123)
+      )
+    )
+  })
+
+  it('parses function application', () => {
+    const tokens = [
+      Token.Def,
+      Token.Var('x'),
+      Token.Equal,
+      Token.Var('f'),
+      Token.Var('x'),
+      Token.Var('y'),
+      Token.Var('z'),
+    ]
+    const bindings = parse(tokens)
+    const expr = bindings[0].expr
+    expect(expr).toEqual(
+      new Apply(
+        new Apply(
+          new Apply(
+            new Var('f'),
+            new Var('x')
+          ),
+          new Var('y')
+        ),
+        new Var('z')
+      )
+    )
+  })
+
+  it('parses dangling lambdas', () => {
+    const tokens = [
+      Token.Def,
+      Token.Var('x'),
+      Token.Equal,
+      Token.Var('f'),
+      Token.BackSlash,
+      Token.Var('x'),
+      Token.Var('x'),
+    ]
+    const bindings = parse(tokens)
+    const expr = bindings[0].expr
+    expect(expr).toEqual(
+      new Apply(
+        new Var('f'),
+        new Lambda(
+          'x',
+          new Var('x')
+        )
+      )
+    )
+  })
+
+  it('parses nested function application', () => {
+    const tokens = [
+      Token.Def,
+      Token.Var('x'),
+      Token.Equal,
+      Token.Var('f'),
+      Token.LeftParen,
+      Token.Var('g'),
+      Token.Var('x'),
+      Token.RightParen,
+    ]
+    const bindings = parse(tokens)
+    const expr = bindings[0].expr
+    expect(expr).toEqual(
+      new Apply(
+        new Var('f'),
+        new Apply(
+          new Var('g'),
+          new Var('x')
+        )
+      )
+    )
+  })
+})
---
+++ src/Parser.ts
@@ -0,0 +1,239 @@
+import { Token, TokenType } from "./Lexer";
+
+export interface Expr {}
+
+export class Binding {
+  name: string
+  expr: Expr
+  constructor(name : string, expr: Expr) {
+    this.name = name
+    this.expr = expr
+  }
+}
+
+export class Var implements Expr {
+  name: string
+  constructor(name: string) {
+    this.name = name
+  }
+}
+
+export class Num implements Expr {
+  value: number
+  constructor(value: number) {
+    this.value = value
+  }
+}
+
+export class Bool implements Expr {
+  value: boolean
+  constructor(value: boolean) {
+    this.value = value
+  }
+}
+
+export class If implements Expr {
+  condition: Expr
+  consequence: Expr
+  alternative: Expr
+  constructor(condition: Expr, consequence: Expr, alternative: Expr) {
+    this.condition = condition
+    this.consequence = consequence
+    this.alternative = alternative
+  }
+}
+
+export class Let implements Expr {
+  name: string
+  value: Expr
+  body: Expr
+  constructor(name: string, value: Expr, body: Expr) {
+    this.name = name
+    this.value = value
+    this.body = body
+  }
+}
+
+export class Lambda implements Expr {
+  param: string
+  body: Expr
+  constructor(param: string, body: Expr) {
+    this.param = param
+    this.body = body
+  }
+}
+
+export class Apply implements Expr {
+  func: Expr
+  argument: Expr
+  constructor(func: Expr, argument: Expr) {
+    this.func = func
+    this.argument = argument
+  }
+}
+
+class ParserError extends Error {}
+
+/**
+ * Describes a program Parser
+ */
+class Parser {
+  #tokens: Token[]
+  #index: number = 0
+  get #current() {
+    return this.#tokens[this.#index]
+  }
+  get #peek() {
+    return this.#tokens[this.#index + 1]
+  }
+  constructor(tokens: Token[]) {
+    this.#tokens = tokens
+  }
+
+  run(): Binding[] {
+    const bindings: Binding[] = []
+    while(this.#current) {
+      switch (this.#current.type) {
+        case Token.Def.type:
+          this.#advance()
+          bindings.push(this.#binding())
+          break;
+      
+        default:
+          throw new ParserError(`[Parser.run] Unexpected token: ${JSON.stringify(this.#current)}`);
+          
+      }
+    }
+    return bindings
+  }
+
+  #binding(): Binding {
+    const name = this.#identifier()
+    this.#expect(TokenType.Equal)
+    const body = this.#expression()
+    return new Binding(name, body)
+  }
+
+  #expression(): Expr {
+    switch (this.#current.type) {
+      case TokenType.If:
+        this.#advance()
+        const condition = this.#expression()
+        this.#expect(TokenType.Then)
+        const consequence = this.#expression()
+        this.#expect(TokenType.Else)
+        const alternative = this.#expression()
+        return new If(condition, consequence, alternative)
+
+      case TokenType.Let:
+        this.#advance()
+        const name = this.#identifier()
+        this.#expect(TokenType.Equal)
+        const value = this.#expression()
+        this.#expect(TokenType.In)
+        const letBody = this.#expression()
+        return new Let(name, value, letBody)
+
+      case TokenType.BackSlash:
+        return this.#lambda()
+    
+      default:
+        return this.#application()
+    }
+  }
+
+  #lambda(): Expr {
+    this.#expect(TokenType.BackSlash)
+    const param = this.#identifier()
+    const lamBody = this.#expression()
+    return new Lambda(param, lamBody)
+  }
+
+  #application(): Expr {
+    // FIXME: There must be a better way than always throwing exceptions for this...
+    let expr = this.#atom()
+    while (this.#current != null) {
+      const index = this.#index
+      try {
+        expr = new Apply(expr, this.#atom())
+      } catch (error) {
+        if (error instanceof ParserError) {
+          this.#index = index
+          break
+        }
+        throw error
+      }
+    }
+    if (this.#current != null && this.#current.type === TokenType.BackSlash) {
+      expr = new Apply(expr, this.#lambda())
+    }
+    return expr
+  }
+
+  #atom(): Expr {
+    let expr
+    switch (this.#current.type) {
+      case TokenType.Var:
+        expr = new Var(this.#current.name)
+        this.#advance()
+        return expr
+    
+      case TokenType.Num:
+        expr = new Num(this.#current.value)
+        this.#advance()
+        return expr
+
+      case TokenType.Bool:
+        expr = new Bool(this.#current.value)
+        this.#advance()
+        return expr
+
+      case TokenType.LeftParen:
+        this.#advance()
+        expr = this.#expression()
+        this.#expect(TokenType.RightParen)
+        return expr
+
+      default:
+        throw new ParserError(`[Parser.atom] Expected atom but got: ${JSON.stringify(this.#current)}`);
+    }
+  }
+
+  #identifier(): string {
+    if (this.#current.type === TokenType.Var) {
+      const name = this.#current.name
+      this.#advance()
+      return name
+    } else {
+      throw new ParserError(`[Parser.identifier] Expected identifier but got: ${JSON.stringify(this.#current)}`);
+    }
+  }
+
+  // Utils
+  #expect(type: TokenType) {
+    if (this.#current.type === type) {
+      this.#advance()
+    } else {
+      throw new ParserError(`[Parser.expect] Expected ${type} but got: ${this.#current}`);
+    }
+  }
+
+  // #accept(type: TokenType): Token | null {
+  //   if (this.#current.type === type) {
+  //     return this.#advance()
+  //   } else {
+  //     return null
+  //   }
+  // }
+
+  #advance(): Token {
+    const token = this.#current
+    this.#index += 1
+    return token
+  }
+}
+
+export function parse(tokens: Token[]): Binding[] {
+  const parser = new Parser(tokens)
+  return parser.run()
+}
